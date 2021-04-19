6cf77ef96d84e892afac717b6c07520e9fd61fe9
Vaishali Rangari <vaishali.rangari@gmail.com>
2021-04-18 22:20:43 -0500

    Updating files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,36 +1,39 @@
-#Deliverable 1
 
 #load dplyr package
 library(dplyr)
 
-#read file
+# Deliverable 1
+# Import the csv file
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-#perform linear regression
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+# Perform Linear Regression
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) 
 
-#perform summary
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+# Summary for the Linear regression model
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) ) 
 
-#Deliverable 2
-#read file
-Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
 
-#get a total summary
-summarize_demo <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
 
-#create a lot summary
-lot_demo <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+# Deliverable 2
+# Import the csv file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#create summary table with multiple columns 
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+#Create lot summary
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
 
 #Deliverable 3
-# t-test for all Lots
-t.test(Suspension$PSI,mu = 1500)
+# t test for all the lots
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
 
-# t-test on Lot 1
-t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+# t test for lot 1
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
 
-# t-test on Lot 2
-t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# t test for lot 2
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
 
-# t-test on Lot 3
-t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+# t test for lot 3
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
